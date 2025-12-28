24a86b686da797106a4a8f26fc04f53c231ec706
UsboKirishima <usbertibox@gmail.com>
2025-12-28 15:21:01 +0100

    core(api): move `app` module to a new folder

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { AppController } from './app.controller'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path'
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from '../users/users.module';
+import { AuthModule } from '../auth/auth.module';
 import { APP_GUARD } from '@nestjs/core'
-import { AuthGuard } from './auth/auth.guard'
+import { AuthGuard } from '../auth/auth.guard'
 import { ConfigifyModule } from '@itgorillaz/configify'
 
 @Module({
@@ -16,7 +16,7 @@ import { ConfigifyModule } from '@itgorillaz/configify'
         GraphQLModule.forRoot<ApolloDriverConfig>({
             driver: ApolloDriver,
             playground: true,
-            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            autoSchemaFile: join(process.cwd(), '../src/schema.gql'),
         }),
         UsersModule,
         AuthModule
